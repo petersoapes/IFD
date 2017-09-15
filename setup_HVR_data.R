caa84445308beee798abe1534c2a278658fe70de
April Peterson <petersoapes@gmail.com>
2017-09-15 13:21:52 -0500

    finished cleaning up Dataset up

--- setup_HVR_data.R
+++ setup_HVR_data.R
@@ -1,16 +1,14 @@
 ## data setup script
 ## input: HVR data files P0 and F2 sheets. Because data was 
 #collected at different stages, they are in a slightly different format. BD's Excel scripts.
-## output: R dataframe with HVR IFD data and BD' MLH1 data
-
+## output: R dataframe with HVR IFD data, BD' MLH1 data, merged HVR_BD_data
 
 library(plyr)
 
 data_HVR_P0 = read.csv("C:/Users/alpeterson7/Documents/HannahVR/HVR_IFD_P0.csv", header=TRUE)
 
 #add REV.tif to end of file name for CAST
-#CAST add '.tif'
-#PWD, remove 'PWD' from begining these files dont have rev
+#CAST add '.tif' #PWD, remove 'PWD' from begining these files dont have rev
 #F2. remove CXP from begining, REV.tif is theme_replace
 data_HVR_P0$File.ID <- ifelse( grepl("CAST", data_HVR_P0$File.ID), paste(data_HVR_P0$File.ID, ".tif", sep=""),
                 ifelse(grepl("PWD", data_HVR_P0$File.ID), substring(data_HVR_P0$File.ID, 4), 
@@ -26,23 +24,18 @@ for(i in 1:length(data_HVR_P0$File.ID)){
   data_HVR_P0$Animal.ID[i] = mouse_list[1] 
 }
 
-#create HVR_full
-#HVR full data set
+#create HVR_full, P0 + F2s
 data_HVR = read.csv("C:/Users/alpeterson7/Documents/HannahVR/HVR_IFD_DATA_Sep17.csv", header=TRUE)
 
 #seperate 2CO from 3CO
-#merge two data sets
-#remove some columns with -c()
 data_HVR <- subset(data_HVR, select = -c(Notes, Date.Collected) )
 colnames(data_HVR) <- c("File.ID", "Cross", "Animal.ID", 
                         "Cell.Count", "n3CO", "Biv.ID", "IFD")
 
-#find file.ID which don't have REV.tif, put into a list
+#merge 2 dataframes
 data_HVR_full <- rbind(data_HVR_P0, data_HVR)
 
-#does cell count need to be filled in for P0s...?
-#fill in n3CO columns
-#this marks 3CO biv (one of the biv measures), as a 3CO with a "1", else everything is NA
+#fill in n3CO columns, #this marks 3CO biv (one of the biv measures), as a 3CO with a "1", else everything is NA
 # this marks the first IFD measure as 3CO (with 1), maybe it should be a X for both IFD measures?
 for(h in 1:length(data_HVR_full$File.ID)){
   #if col 6 of 7 match and 7th(IFD doesn't ) 3CO  
@@ -61,84 +54,40 @@ nonrv <- data_HVR_full$File.ID[!grepl("REV", data_HVR_full$File.ID)]
 nonrv <- unique(nonrv) #42 biv measures without REV
 nonrv <- gsub('.tif', '', nonrv)#remove '.tif' for better matching
 
-#LOAD BD data (why did I have two seperate files for BD data)
 
+#LOAD BD data (why did I have two seperate files for BD data)
 BD_data = read.csv("C:/Users/alpeterson7/Documents/HannahVR/BD_RecombinationPhenotypes.csv", header=TRUE)
 #there are only 10 PWD in this excel sheet, but 17 PWD mouse folders on lacie. First 7folders don't contain 'REV'
 #suffix.
-#remove all strains but PWD, CAST and CXP PXC, PxCF2, PxCF1, WSBxCASTF1, CxPF1, CxPF2
 
+#remove all strains but PWD, CAST and CXP PXC, PxCF2, PxCF1, WSBxCASTF1, CxPF1, CxPF2, CZCHEI
 BD_data <- BD_data[( (BD_data$Cross == "CxPF2") | (BD_data$Cross == "PxCF1") 
                 | (BD_data$Cross == "CZECHI") | (BD_data$Cross == "PWD") 
                 | (BD_data$Cross == "CAST") | (BD_data$Cross == "PxCF2")  ), ]
 
-#subset BD data (don't include )
+#subset BD data. columns not to include
 BD_data <- subset(BD_data, select= -c(InchWidth, InchHeight, PixelHeight, PixelWidth, RawDistFromCent, FractDistFromCent))
-#InchWidth, InchHeight, PixelHeight, PixelWidth, RawDistFromCent
-#FractDistFromCent
+#InchWidth, InchHeight, PixelHeight, PixelWidth, RawDistFromCent#FractDistFromCent
 
 #create file name col for merging with HVR data
 BD_data$file_name <- do.call(paste, c(BD_data[c("ANIMAL_ID", "Slide_ID", "CellNumber")], sep = "_")) 
 
-#don't know if this is working
-REV <- subset(BD_data$file_name, !(BD_data$file_name %in% nonrv) ) #this returns a matrix of T OR F
-length(REV)#same length as full dataframe
-#IT seems like my versions of BD's data don't have the nonREV data... most 'file names should match (ie are useful for merging)
+#add REV to correct cells
+REV <- subset(BD_data$file_name, !(BD_data$file_name %in% nonrv) )
+#length(REV)#same length as full dataframe
 
-#can't find the mice, (2144 and 2146 in BD's df), 2146 and 2605 are the two PWD in HVR
-# there should be 3 HVR PWD mice, they should show up in BD data
-BD_MLH1_data$file_name <- paste( (BD_MLH1_data$file_name[!(BD_MLH1_data$file_name %in% nonrv)]) , "_REV.tif", sep="")
+BD_data$file_name <- paste( (BD_data$file_name[!(BD_data$file_name %in% nonrv)]) , "_REV.tif", sep="")
 
-#BD_MLH1_data <- subset(BD_MLH1_data, select = -c(file_name) )#$FractDistFromCent, RawDistFromCent
-#TODO for merge file, make sure cross categories are consistent, make sure file names are correct
-IFD_nMLH1 <- merge.data.frame(BD_MLH1_data, data_HVR_full, by.y = "File.ID", by.x = "file_name", all=FALSE)
+#merge HVR and BD dataframes, make sure cross categories are consistent, make sure file names are correct
+IFD_nMLH1 <- merge.data.frame(BD_data, data_HVR_full, by.y = "File.ID", by.x = "file_name", all=FALSE)
 
-# subset some data
-data_PWD <- IFD_nMLH1[IFD_nMLH1$Cross.x == "PWD", ] #the file names for many PWD don't match
-data_CAST <- IFD_nMLH1[IFD_nMLH1$Cross.x == "CAST", ]
-data_F2 <- IFD_nMLH1[IFD_nMLH1$Cross.x == "CXPF2", ]
-
-#Make some tables for basic stats. (number of categories)
-category_numbers_full <- ddply(IFD_nMLH1, .(Cross.x), summarize,
-                          N_mice  = length(unique(Animal.ID)),#number of measurements
-                          #N_IFD_MLH1 = ,
-                          mean_MLH1 = mean(IFD),
-                          var_IFD = var(IFD),#this is very large within mouse
-                          sd_IFD   = sd(IFD)
-                          #se   = sd / sqrt(N)
-)
-category_numbers_full
-#3 from HVR data and 10 from BD data
-
-category_numbers_HVR <- ddply(data_HVR_full, .(Cross), summarize,
-                               N_mice  = length(unique(Animal.ID)),#number of measurements
-                               #N_IFD_MLH1 = ,
-                               mean_MLH1 = mean(IFD),
-                               var_IFD = var(IFD),#this is very large within mouse
-                               sd_IFD   = sd(IFD)
-                               #se   = sd / sqrt(N)
-)
-category_numbers_HVR
-
-#BDs lacie folder has 17 PWD folders, not sure why it's not in Excel sheet
-category_numbers_BD_full <- ddply(BD_data, .(Cross), summarize,
-                               N_mice  = length(unique(ANIMAL_ID))#number of measurements
-                               #N_IFD_MLH1 = ,
-                              # mean_MLH1 = mean(IFD),
-                              # var_IFD = var(IFD),#this is very large within mouse
-                              # sd_IFD   = sd(IFD)
-                               #se   = sd / sqrt(N)
-)
-category_numbers_BD_full
-
+# CLEAN UP
+#don't remove the user specific data set since the PWD measures don't comppletely overlap
+rm(data_HVR)
+rm(data_HVR_P0)
 
 #FINAL 
 ## save data
 setwd("C:/Users/alpeterson7/Documents/HannahVR/HVRrepo/")
 save.image("HVR_data_setup.RData")
 
-# CLEAN UP
-#save this list and use it to not add "REV" to when processing BD data
-#don't remove the user specific data set since the PWD measures don't comppletely overlap
-#rm(data_HVR)
-#rm(data_HRV_P0)
